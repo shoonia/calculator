230ed3e41f7602bfa9089e452f814239a776b0e2
Aleksandr Zaytsev <zaytsev126@gmail.com>
2017-02-10 21:28:02 +0200

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -2,29 +2,30 @@
 var tablo = document.getElementById('tablo'),
     panel = document.getElementById('panel');
 
-panel.addEventListener('click', function( event ) {
-    
-    'use strict';
+panel.addEventListener( 'click', function (event) {
 	
-	var data = event.target.value || window.event.srcElement.value;
+	'use strict';
+	
+	var data = event.target.value;
 
-	if( !data ) return;
+	if (!data) return;
  
-	if( data == 'clean' ) {
+	if (data == 'clean') {
 		tablo.value = '';
 		
-	} else if( data == 'total' && tablo.value ) {
+	} else if (data == 'total' && tablo.value) {
+		
 		try {
 			tablo.value = eval( tablo.value );
             
 		} catch(e) {
-			console.log( e );
+			console.log(e);
 		}
         
-	} else if( data == 'deletion' ) {
+	} else if (data == 'deletion') {
 		tablo.value = tablo.value.slice(0, -1);
 		
-	} else if( data != 'total' ) {
+	} else if (data != 'total') {
 		tablo.value += data;
 	}
 });
