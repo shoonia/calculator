5a00ea384167de87e9c04f9b2cce6173addb87a8
Aleksandr Zaytsev <zaytsev126@gmail.com>
2017-06-01 22:50:30 +0300

    Formatter

--- js/calculator.js
+++ js/calculator.js
@@ -1,29 +1,27 @@
-
-var tablo = document.getElementById('tablo'),
-    panel = document.getElementById('panel');
+const tablo = document.getElementById('tablo');
+const panel = document.getElementById('panel');
 
 panel.addEventListener( 'click', function (event) {
-	
-	'use strict';
-	
-	var data = event.target.value;
 
-	if (!data) return;
- 
-	if (data == 'clean') {
-		tablo.value = '';
-		
-	} else if (data == 'total' && tablo.value) {
-		
-		try {
-			tablo.value = eval( tablo.value );
-            
-		} catch (e) {}
-        
-	} else if (data == 'deletion') {
-		tablo.value = tablo.value.slice( 0, -1 );
-		
-	} else if (data != 'total') {
-		tablo.value += data;
-	}
+  'use strict';
+
+  let data = event.target.value;
+
+  if (!data) return;
+
+  if (data === 'clean') {
+    tablo.value = '';
+
+  } else if (data === 'total' && tablo.value) {
+
+    try {
+      tablo.value = eval( tablo.value );
+    } catch (e) {}
+
+  } else if (data === 'deletion') {
+    tablo.value = tablo.value.slice( 0, -1 );
+
+  } else if (data !== 'total') {
+    tablo.value += data;
+  }
 });
