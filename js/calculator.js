90990bccacb851bbf621db5b539009ff3106a41a
Zaytsev Sasha <zaytsev126@gmail.com>
2018-02-10 19:05:22 +0200

    Modul

--- js/calculator.js
+++ js/calculator.js
@@ -1,25 +1,33 @@
-const tablo = document.getElementById('tablo');
-const panel = document.getElementById('panel');
-
-panel.addEventListener('click', function (event) {
+(function (doc) {
     'use strict';
 
-    let data = event.target.value;
+    var tablo = doc.getElementById('tablo');
+    var panel = doc.getElementById('panel');
 
-    if (!data) return;
+    panel.addEventListener('click', function (event) {
 
-    if (data === 'clean') {
-        tablo.value = '';
+        var data = event.target.value;
 
-    } else if (data === 'total' && tablo.value !== '') {
-        try {
-            tablo.value = eval( tablo.value );
-        } catch (e) {}
+        if (!data) return;
 
-    } else if (data === 'deletion') {
-        tablo.value = tablo.value.slice( 0, -1 );
+        if (data === 'clean') {
+            tablo.value = '';
 
-    } else if (data !== 'total') {
-        tablo.value += data;
-    }
-});
+        } else if (data === 'total' && tablo.value !== '') {
+            try {
+                var calc = new Function('return ' + tablo.value);
+                tablo.value = calc();
+
+            } catch (e) {
+                console.log(e);
+            }
+
+        } else if (data === 'deletion') {
+            tablo.value = tablo.value.slice( 0, -1 );
+
+        } else if (data !== 'total') {
+            tablo.value += data;
+        }
+    });
+
+})(document);
